598bdcc5506397a59b2922f3765a6ee9fedb7196
Arham <mohdarham0634@gmail.com>
2023-09-21 02:22:05 +0530

    modified

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ function addNewTask() {
 
     let task = {
         value: taskValue,
-        checked: false
+        checked: false,
+        color: `#${Math.floor(Math.random()*16777215).toString(16)}`
     }
     tasksArray.push(task)
 
@@ -59,7 +60,6 @@ function addNewTask() {
     newTaskInput.value = '';
     saveTaskList(tasksArray)
 }
-
 
 
 // render
@@ -74,7 +74,7 @@ function renderTasks() {
 
         taskItem.innerHTML = `
             <div class="content">
-                <svg data-action="check" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#ffffff">
+                <svg data-action="check" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="${task.color}">
                     <path data-action="check" ${task.checked ? 'd="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"'
                     : 'd="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"'}
                     />
